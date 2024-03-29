6898b4d8d3f698de4c2dbeb0bda8cb3b5ea81457
Johan Alejandro Peña <johanpena998@gmail.com>
2024-03-29 16:05:06 -0300

    firts commit

---
+++ src/componentes/SocialButtom.jsx
@@ -0,0 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SocialButtom = ({ icon }) => {
+  return <FontAwesomeIcon icon={icon} />;
+};
+
+export default SocialButtom;
